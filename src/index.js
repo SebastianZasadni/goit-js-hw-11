6b6ed18e4cf25ca6383d92c1e1c0d066dfa1e0d2
SebastianZasadni <emblelele@gmail.com>
2023-03-09 20:23:11 +0100

    feat task-11

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+const form = document.querySelector("#search-form");
+const gallery = document.querySelector('.gallery');
+
+const sendRequest = async(event) => {
+    event.preventDefault();
+    let {
+        elements: { searchQuery }
+      } = event.currentTarget;
+    searchQuery = searchQuery.value;
+   
+    await makeGallery(searchQuery);
+  };
+
+form.addEventListener("submit", sendRequest);
+
+const fetchImages = async(searchQuery) => {
+    const response = await axios.get(`https://pixabay.com/api/?key=34267096-8e89032011ab6ec20673badb0&q=${searchQuery}&image_type=photo&orientation=horizontal&safeSearch=true`);
+    const images = response.data;
+    return images;
+};
+
+const makeGallery = async(searchQuery) => {
+    try {
+        let images = await fetchImages(searchQuery);
+        images = images.hits;
+        const markup = images
+        .map(image => {
+            return `<div class="photo-card">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" class="photo-item"/>
+            <div class="info">
+              <p class="info-item">
+                <b>Likes</b>
+                ${image.likes}
+              </p>
+              <p class="info-item">
+                <b>Views</b>
+                ${image.views}
+              </p>
+              <p class="info-item">
+                <b>Comments</b>
+                ${image.comments}
+              </p>
+              <p class="info-item">
+                <b>Download</b>
+                ${image.downloads}
+              </p>
+            </div>
+          </div>`;
+        })
+        .join('');
+        gallery.innerHTML = markup;
+           }
+    catch (error) {
+     console.log(error.message);
+    }
+}
+
+
+
